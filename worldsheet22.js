b6451962b786d244793fc6aa77b897edb5fd560c
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-04-02 00:58:39 +0800

    Add files via upload

---
+++ worldsheet22.js
@@ -0,0 +1,42 @@
+void function worldsheet22(){
+export const sheet1=([[1][0],
+                              [0][0]])
+
+export const sheet2 =([[0][1],
+                                [0][0]])
+
+export const sheet3=([[0][0],
+                               [1][0]])
+
+export const sheet4=([[1][0],
+                    [0][0]])
+    R
+export const sheet5=([[1][1],
+                                [0][0]])
+
+export const sheet6=([[0][1],
+                              [0][1]])
+
+export const sheet7 =([[0][0],
+                                [1][1]])
+
+export const sheet8=([[1][0],
+                               [1][0]])
+
+export const sheet9=([[1][1],
+                               [0][1]])
+export const sheet10=([[0][1],
+                                [1][1]])
+
+export const sheet11=([[1][0],
+                                [1][1]])
+
+export const sheet12=([[1][1],
+         [1][0]])
+
+export const sheet13=([[1][1],
+                               [1][1]])
+    return[sheet1,sheet2,sheet3,sheet4,sheet5,
+           sheet6,sheet7,sheet8,sheet9,sheet10,
+           sheet11,sheet12,sheet13]
+}
